fe18831291ec3e94ca2434c6b61d6c2b71ac186a
Foresteam <heler1141@gmail.com>
2022-04-22 20:53:29 +0300

    Architecture

--- src/Reader.ts
+++ src/Reader.ts
@@ -3,133 +3,138 @@ import fs from 'fs';
 import { TYPES, TYPE, Entry, NBTType } from './Common';
 import { XMLBuilder } from 'xmlbuilder2/lib/interfaces';
 
-const GetName = (pos: number, bytes: Buffer, headless: boolean) => {
-	if (headless)
-		return { name: null, offset: 0 };
-	let len = bytes.readUInt16BE(pos);
-	return {
-		name: bytes.toString('utf-8', pos + 2, pos + 2 + len),
-		offset: 2 + len
-	};
-};
-
 interface RSReturn {
 	entry: Entry;
 	endPos: number;
 	name?: string;
 };
-const ReadNumber = (buffer: Buffer, pos: number, type: number, headless = false): RSReturn => {
-	let self: Entry = { type, value: null };
-	let { name, offset: nameOffset } = GetName(pos, buffer, headless);
-	pos += nameOffset;
-	switch (type) {
-		case TYPE('byte'):
-			self.value = buffer.readInt8(pos);
-			pos += 1;
-			break;
-		case TYPE('short'):
-			self.value = buffer.readInt16BE(pos);
-			pos += 2;
-			break;
-		case TYPE('int'):
-			self.value = buffer.readInt32BE(pos);
-			pos += 4;
-			break;
-		case TYPE('long'):
-			self.value = buffer.readBigUInt64BE(pos);
-			pos += 8;
-			break;
-		case TYPE('float'):
-			self.value = buffer.readFloatBE(pos);
-			pos += 4;
-			break;
-		case TYPE('double'):
-			self.value = buffer.readDoubleBE(pos);
-			pos += 8;
-			break;
+class Reader {
+	buffer: Buffer;
+	constructor(buffer: Buffer) {
+		this.buffer = buffer;
 	}
-	return { entry: self, endPos: pos, name };
-};
-const ReadString = (buffer: Buffer, pos: number, type: number, headless = false): RSReturn => {
-	let self: Entry = { type, value: null };
-	let { name, offset: nameOffset } = GetName(pos, buffer, headless);
-	pos += nameOffset;
-	let len = buffer.readUInt16BE(pos);
-	pos += 2;
-	self.value = buffer.toString('utf-8', pos, pos + len);
-	return { entry: self, endPos: pos + len, name };
-};
-const ReadArray = (buffer: Buffer, pos: number, type: number, headless = false): RSReturn => {
-	let self: Entry = { type, value: [], contentType: TYPE(TYPES[type].replace('[]', '') as NBTType) };
-	let { name, offset: nameOffset } = GetName(pos, buffer, headless);
-	pos += nameOffset;
-	let len = buffer.readInt32BE(pos);
-	pos += 4;
-	for (let i = 0; i < len; i++) {
-		let entryInfo = ReadNumber(buffer, pos, self.contentType, true);
-		(self.value as Entry[]).push(entryInfo.entry);
-		pos = entryInfo.endPos;
+	GetName(pos: number, headless: boolean) {
+		if (headless)
+			return { name: null, offset: 0 };
+		let len = this.buffer.readUInt16BE(pos);
+		return {
+			name: this.buffer.toString('utf-8', pos + 2, pos + 2 + len),
+			offset: 2 + len
+		};
 	}
-	return { entry: self, endPos: pos, name }
-};
-
-const _TheSwitch = (buffer: Buffer, pos: number, type: number, headless: boolean): RSReturn => {
-	let entryInfo: RSReturn;
-	if (type <= TYPE('double'))
-		entryInfo = ReadNumber(buffer, pos, type, headless);
-	else if (type == TYPE('string'))
-		entryInfo = ReadString(buffer, pos, type, headless);
-	else if (type == TYPE('list'))
-		entryInfo = ReadList(buffer, pos, type, headless);
-	else if (type == TYPE('compound'))
-		entryInfo = ReadCompound(buffer, pos, type, headless);
-	else if (type == TYPE('byte[]') || type >= TYPE('int[]'))
-		entryInfo = ReadArray(buffer, pos, type, headless);
-	return entryInfo;
-}
-const ReadList = (buffer: Buffer, pos: number, _type: number, headless = false): RSReturn => {
-	let self: Entry = { value: [], type: _type };
-	let { name, offset: nameOffset } = GetName(pos, buffer, headless);
-	pos += nameOffset;
-	// TYPE('byte')
-	self.contentType = buffer.readInt8(pos);
-	pos++
-	// TYPE('int')
-	let contentLength = buffer.readInt32BE(pos);
-	pos += 4;
-	for (let i = 0; i < contentLength; i++) {
-		let entryInfo = _TheSwitch(buffer, pos, self.contentType, true);
-		if (entryInfo) {
+	ReadNumber(pos: number, type: number, headless = false): RSReturn {
+		let self: Entry = { type, value: null };
+		let { name, offset: nameOffset } = this.GetName(pos, headless);
+		pos += nameOffset;
+		switch (type) {
+			case TYPE('byte'):
+				self.value = this.buffer.readInt8(pos);
+				pos += 1;
+				break;
+			case TYPE('short'):
+				self.value = this.buffer.readInt16BE(pos);
+				pos += 2;
+				break;
+			case TYPE('int'):
+				self.value = this.buffer.readInt32BE(pos);
+				pos += 4;
+				break;
+			case TYPE('long'):
+				self.value = this.buffer.readBigUInt64BE(pos);
+				pos += 8;
+				break;
+			case TYPE('float'):
+				self.value = this.buffer.readFloatBE(pos);
+				pos += 4;
+				break;
+			case TYPE('double'):
+				self.value = this.buffer.readDoubleBE(pos);
+				pos += 8;
+				break;
+		}
+		return { entry: self, endPos: pos, name };
+	};
+	ReadString(pos: number, type: number, headless = false): RSReturn {
+		let self: Entry = { type, value: null };
+		let { name, offset: nameOffset } = this.GetName(pos, headless);
+		pos += nameOffset;
+		let len = this.buffer.readUInt16BE(pos);
+		pos += 2;
+		self.value = this.buffer.toString('utf-8', pos, pos + len);
+		return { entry: self, endPos: pos + len, name };
+	};
+	ReadArray(pos: number, type: number, headless = false): RSReturn {
+		let self: Entry = { type, value: [], contentType: TYPE(TYPES[type].replace('[]', '') as NBTType) };
+		let { name, offset: nameOffset } = this.GetName(pos, headless);
+		pos += nameOffset;
+		let len = this.buffer.readInt32BE(pos);
+		pos += 4;
+		for (let i = 0; i < len; i++) {
+			let entryInfo = this.ReadNumber(pos, self.contentType, true);
 			(self.value as Entry[]).push(entryInfo.entry);
 			pos = entryInfo.endPos;
 		}
-		else
-			break;
-	}
-	return { entry: self, endPos: pos, name };
-}
-/** @param pos The buffer offset. The tag type is assumed to be already known, so it must be excluded */
-const ReadCompound = (buffer: Buffer, pos: number, _type: number, headless = false): RSReturn => {
-	let self: Entry = { value: {}, type: _type };
-	let { name, offset: nameOffset } = GetName(pos, buffer, headless);
-	pos += nameOffset;
-	while (pos < Buffer.byteLength(buffer)) {
-		let type: number = buffer.readUInt8(pos);
-		if (type == TYPE('end')) {
-			pos++;
-			break;
-		}
+		return { entry: self, endPos: pos, name }
+	};
 
-		let entryInfo = _TheSwitch(buffer, pos + 1, type, false);
-		if (entryInfo) {
-			self.value[entryInfo.name] = entryInfo.entry;
-			pos = entryInfo.endPos;
-			continue;
-		}
-		pos++;
+	_TheSwitch(pos: number, type: number, headless: boolean): RSReturn {
+		let entryInfo: RSReturn;
+		if (type <= TYPE('double'))
+			entryInfo = this.ReadNumber(pos, type, headless);
+		else if (type == TYPE('string'))
+			entryInfo = this.ReadString(pos, type, headless);
+		else if (type == TYPE('list'))
+			entryInfo = this.ReadList(pos, type, headless);
+		else if (type == TYPE('compound'))
+			entryInfo = this.ReadCompound(pos, type, headless);
+		else if (type == TYPE('byte[]') || type >= TYPE('int[]'))
+			entryInfo = this.ReadArray(pos, type, headless);
+		return entryInfo;
 	}
-	return { entry: self, endPos: pos, name };
-};
+	ReadList(pos: number, _type: number, headless = false): RSReturn {
+		let self: Entry = { value: [], type: _type };
+		let { name, offset: nameOffset } = this.GetName(pos, headless);
+		pos += nameOffset;
+		// TYPE('byte')
+		self.contentType = this.buffer.readInt8(pos);
+		pos++
+		// TYPE('int')
+		let contentLength = this.buffer.readInt32BE(pos);
+		pos += 4;
+		for (let i = 0; i < contentLength; i++) {
+			let entryInfo = this._TheSwitch(pos, self.contentType, true);
+			if (entryInfo) {
+				(self.value as Entry[]).push(entryInfo.entry);
+				pos = entryInfo.endPos;
+			}
+			else
+				break;
+		}
+		return { entry: self, endPos: pos, name };
+	}
+	/** @param pos The buffer offset. The tag type is assumed to be already known, so it must be excluded */
+	ReadCompound(pos: number, _type: number, headless = false): RSReturn {
+		let self: Entry = { value: {}, type: _type };
+		let { name, offset: nameOffset } = this.GetName(pos, headless);
+		pos += nameOffset;
+		while (pos < Buffer.byteLength(this.buffer)) {
+			let type: number = this.buffer.readUInt8(pos);
+			if (type == TYPE('end')) {
+				pos++;
+				break;
+			}
+
+			let entryInfo = this._TheSwitch(pos + 1, type, false);
+			if (entryInfo) {
+				self.value[entryInfo.name] = entryInfo.entry;
+				pos = entryInfo.endPos;
+				continue;
+			}
+			pos++;
+		}
+		return { entry: self, endPos: pos, name };
+	};
+}
 
 const BuildXML = (block: Entry, parent: XMLBuilder, root = false, name?: string): void => {
 	if (typeof block.value != 'object') {
@@ -164,7 +169,7 @@ export default {
 	// 'tests/test.dat.uncompressed'
 	ReadNBT(filename: string): Entry {
 		let nbtBytes: Buffer = fs.readFileSync(filename);
-		return ReadCompound(nbtBytes, 1, TYPE('compound')).entry;
+		return new Reader(nbtBytes).ReadCompound(1, TYPE('compound')).entry;
 	},
 	// 'tests/lol.xml'
 	WriteXML(filename: string, data: Entry) {
--- src/Writer.ts
+++ src/Writer.ts
@@ -2,6 +2,100 @@ import fs from 'fs';
 import { XMLParser } from 'fast-xml-parser';
 import { TYPES, TYPE, IS_INLINE, IS_ARRAY, Entry, NBTType } from './Common';
 
+class Writer {
+	out: fs.WriteStream;
+	constructor(out: fs.WriteStream) {
+		this.out = out;
+	}
+
+	WriteBuf(buf: Buffer) {
+		return new Promise(resolve => this.out.write(buf, 'binary', resolve));
+	}
+	async WriteTypeAndName(type: number, name?: string, headless = false) {
+		if (headless)
+			return;
+		let nameLen = name ? Buffer.from(name, 'utf-8').byteLength : 0;
+		let buf = Buffer.alloc(1 + 2 + nameLen);
+		buf.writeUInt8(type);
+		buf.writeUInt16BE(nameLen, 1);
+		if (name)
+			buf.write(name, 3, 'utf-8');
+		await this.WriteBuf(buf);
+	}
+	async WriteInline(type: number, entry: Entry, name?: string, headless = false) {
+		await this.WriteTypeAndName(type, name, headless);
+		let buf: Buffer;
+		if (type == TYPE('byte')) {
+			buf = Buffer.alloc(1);
+			buf.writeInt8(Number(entry.value));
+		}
+		if (type == TYPE('short')) {
+			buf = Buffer.alloc(2);
+			buf.writeInt16BE(entry.value);
+		}
+		if (type == TYPE('int')) {
+			buf = Buffer.alloc(4);
+			buf.writeInt32BE(entry.value);
+		}
+		if (type == TYPE('long')) {
+			buf = Buffer.alloc(8);
+			buf.writeBigInt64BE(entry.value);
+		}
+		if (type == TYPE('float')) {
+			buf = Buffer.alloc(4);
+			buf.writeFloatBE(entry.value);
+		}
+		if (type == TYPE('double')) {
+			buf = Buffer.alloc(8);
+			buf.writeDoubleBE(entry.value);
+		}
+		if (type == TYPE('string')) {
+			let len = Buffer.from(entry.value, 'utf-8').byteLength;
+			buf = Buffer.alloc(2 + len);
+			buf.writeUInt16BE(len);
+			buf.write(entry.value, 2, 'utf-8');
+		}
+		await this.WriteBuf(buf);
+	}
+	async WriteArray(entry: Entry, name?: string, headless = false) {
+		await this.WriteTypeAndName(entry.type, name, headless);
+		let length = Buffer.alloc(4);
+		length.writeInt32BE((entry.value as Entry[]).length);
+		await this.WriteBuf(length);
+		for (let v of entry.value as Entry[])
+			await this.WriteInline(entry.contentType, v, null, true);
+	}
+	async WriteSwitch(type: number, entry: Entry, name?: string, headless = false) {
+		if (IS_INLINE(entry.type))
+			await this.WriteInline(type, entry, name, headless);
+		if (IS_ARRAY(entry.type))
+			await this.WriteArray(entry, name, headless);
+		if (entry.type == TYPE('list'))
+			await this.WriteList(entry, name, headless);
+		if (TYPES[entry.type] == 'compound')
+			await this.WriteCompound(entry, name, headless);
+	}
+	async WriteList(entry: Entry, name?: string, headless = false) {
+		await this.WriteTypeAndName(entry.type, name, headless);
+		let info = Buffer.alloc(1 + 4);
+		info.writeUInt8(entry.contentType);
+		info.writeInt32BE((entry.value as Entry[]).length, 1);
+		await this.WriteBuf(info);
+		for (let v of entry.value as Entry[])
+			await this.WriteSwitch(entry.contentType, v, null, true);
+	}
+	// headless = !!name
+	async WriteCompound(entry: Entry, name?: string, headless = false) {
+		await this.WriteTypeAndName(TYPE('compound'), name, headless);
+		for (let [name, child] of Object.entries(entry.value) as [string, Entry][])
+			await this.WriteSwitch(child.type, child, name);
+		// WriteInline(TYPE('byte'), , 'seenCredits');
+		let buf = Buffer.alloc(1);
+		buf.writeUInt8(TYPE('end'));
+		await this.WriteBuf(buf);
+	}
+}
+
 interface XMLEntryDescriptor {
 	name: string;
 	of: string;
@@ -32,91 +126,6 @@ const Entrify = (tag: XMLEntry): [string | number, Entry] => {
 	return [tag[':@']?.name, entry];
 }
 
-const WriteBuf = (out: fs.WriteStream, buf: Buffer) => new Promise(resolve => out.write(buf, 'binary', resolve));
-const WriteTypeAndName = async (out: fs.WriteStream, type: number, name?: string, headless = false) => {
-	if (headless)
-		return;
-	let nameLen = name ? Buffer.from(name, 'utf-8').byteLength : 0;
-	let buf = Buffer.alloc(1 + 2 + nameLen);
-	buf.writeUInt8(type);
-	buf.writeUInt16BE(nameLen, 1);
-	if (name)
-		buf.write(name, 3, 'utf-8');
-	await WriteBuf(out, buf);
-}
-const WriteInline = async (out: fs.WriteStream, type: number, entry: Entry, name?: string, headless = false) => {
-	await WriteTypeAndName(out, type, name, headless);
-	let buf: Buffer;
-	if (type == TYPE('byte')) {
-		buf = Buffer.alloc(1);
-		buf.writeInt8(Number(entry.value));
-	}
-	if (type == TYPE('short')) {
-		buf = Buffer.alloc(2);
-		buf.writeInt16BE(entry.value);
-	}
-	if (type == TYPE('int')) {
-		buf = Buffer.alloc(4);
-		buf.writeInt32BE(entry.value);
-	}
-	if (type == TYPE('long')) {
-		buf = Buffer.alloc(8);
-		buf.writeBigInt64BE(entry.value);
-	}
-	if (type == TYPE('float')) {
-		buf = Buffer.alloc(4);
-		buf.writeFloatBE(entry.value);
-	}
-	if (type == TYPE('double')) {
-		buf = Buffer.alloc(8);
-		buf.writeDoubleBE(entry.value);
-	}
-	if (type == TYPE('string')) {
-		let len = Buffer.from(entry.value, 'utf-8').byteLength;
-		buf = Buffer.alloc(2 + len);
-		buf.writeUInt16BE(len);
-		buf.write(entry.value, 2, 'utf-8');
-	}
-	await WriteBuf(out, buf);
-}
-const WriteArray = async (out: fs.WriteStream, entry: Entry, name?: string, headless = false) => {
-	await WriteTypeAndName(out, entry.type, name, headless);
-	let length = Buffer.alloc(4);
-	length.writeInt32BE((entry.value as Entry[]).length);
-	await WriteBuf(out, length);
-	for (let v of entry.value as Entry[])
-		await WriteInline(out, entry.contentType, v, null, true);
-}
-const WriteSwitch = async (out: fs.WriteStream, type: number, entry: Entry, name?: string, headless = false) => {
-	if (IS_INLINE(entry.type))
-		await WriteInline(out, type, entry, name, headless);
-	if (IS_ARRAY(entry.type))
-		await WriteArray(out, entry, name, headless);
-	if (entry.type == TYPE('list'))
-		await WriteList(out, entry, name, headless);
-	if (TYPES[entry.type] == 'compound')
-		await WriteCompound(out, entry, name, headless);
-}
-const WriteList = async (out: fs.WriteStream, entry: Entry, name?: string, headless = false) => {
-	await WriteTypeAndName(out, entry.type, name, headless);
-	let info = Buffer.alloc(1 + 4);
-	info.writeUInt8(entry.contentType);
-	info.writeInt32BE((entry.value as Entry[]).length, 1);
-	await WriteBuf(out, info);
-	for (let v of entry.value as Entry[])
-		await WriteSwitch(out, entry.contentType, v, null, true);
-}
-// headless = !!name
-const WriteCompound = async (out: fs.WriteStream, entry: Entry, name?: string, headless = false) => {
-	await WriteTypeAndName(out, TYPE('compound'), name, headless);
-	for (let [name, child] of Object.entries(entry.value) as [string, Entry][])
-		await WriteSwitch(out, child.type, child, name);
-	// WriteInline(TYPE('byte'), , 'seenCredits');
-	let buf = Buffer.alloc(1);
-	buf.writeUInt8(TYPE('end'));
-	await WriteBuf(out, buf);
-}
-
 export default {
 	ReadXML(filename: string): Entry {
 		let parser = new XMLParser({ preserveOrder: true, ignoreAttributes: false, attributeNamePrefix: "", allowBooleanAttributes: true, ignoreDeclaration: true });
@@ -126,7 +135,7 @@ export default {
 	// 'tests/result.dat.uncompressed'
 	async WriteNBT(filename: string, root: Entry): Promise<void> {
 		let nbtOut = fs.createWriteStream(filename, 'binary');
-		await WriteCompound(nbtOut, root);
+		await new Writer(nbtOut).WriteCompound(root);
 		await nbtOut.close();
 	}
 }
