67381db88b5d7438f44b40f7e1899eb31acd41a5
Foresteam <heler1141@gmail.com>
2022-07-07 05:10:59 +0300

    Decomposition, new arrow icons

---
+++ src/components/ModeSwitch.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="mode-switch">
+		<i class="pi fi fi-package icon">
+			<span>NBT</span>
+		</i>
+		<div style="margin-left: 30pt; margin-right: 30pt;">
+			<p-toggle-button
+				:modelValue="modelValue"
+				@update:modelValue="(v: boolean) => onChange(v)"
+				onIcon="pi fi fi-arrow-left"
+				offIcon="pi fi fi-arrow-right"
+				class="button-bigtext"
+				style="width: 100%"
+			/>
+		</div>
+		<i class="pi fi fi-file icon">
+			<span>XML</span>
+		</i>
+	</div>
+</template>
+
+<style>
+.mode-switch i:before {
+	font-size: 70pt;
+}
+.mode-switch .p-togglebutton,
+.mode-switch .p-togglebutton * {
+	transition: .2s ease-out;
+	border: none !important;
+	background: transparent !important;
+	color: var(--primary-color) !important;
+	box-shadow: none !important;
+	font-size: 60pt !important;
+}
+.mode-switch .p-togglebutton:hover * {
+	color: var(--primary-color-highlight) !important;
+}
+.mode-switch > .icon > * {
+	position: absolute;
+	display: inline;
+	margin-top: 75pt;
+	font-weight: bold;
+	font-size: 20pt;
+	color: var(--gray-400);
+}
+
+.mode-switch > .icon {
+	display: flex;
+	justify-content: center;
+	padding-bottom: 35pt
+}
+</style>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{ modelValue: boolean }>();
+const emit = defineEmits(['update:modelValue', 'change']);
+
+const onChange = (v: boolean) => {
+	emit('change', v);
+	emit('update:modelValue', v);
+};
+</script>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -12,23 +12,7 @@
 			v-model="input"
 			mode="open"
 		/>
-		<div class="flex-row flex-center mode-switch ui-block-v" style="">
-			<i class="pi fi fi-package icon">
-				<span>NBT</span>
-			</i>
-			<div style="margin-left: 30pt; margin-right: 30pt;">
-				<p-toggle-button
-					v-model="xmlinput"
-					onIcon="pi pi-arrow-left"
-					offIcon="pi pi-arrow-right"
-					class="button-bigtext"
-					style="width: 100%"
-				/>
-			</div>
-			<i class="pi fi fi-file icon">
-				<span>XML</span>
-			</i>
-		</div>
+		<mode-switch v-model="xmlinput" class="flex-row flex-center ui-block-v" />
 		<div class="flex-row ui-block" style="align-items: center">
 			<p-tri-state-checkbox id="compression-checkbox" v-model="compression" :disabled="xmlinput" />
 			<label v-if="compression == null" for="compression-checkbox">{{ locales['Main.compression-checkbox.0'] }}</label>
@@ -76,19 +60,29 @@
 	</div>
 </template>
 
+<style>
+.p-button-success:not(:hover) .p-button-icon {
+	transition: .5s ease-out;
+	color: var(--primary-color-text) !important;
+}
+
+.home {
+	width: 100%;
+}
+</style>
+
 <script lang="ts">
 import { defineComponent } from '@vue/runtime-core';
 import '@/shared/IPCTypes';
 import FileInput from '@/components/FileInput.vue';
+import ModeSwitch from '@/components/ModeSwitch.vue'
 import { mapState, mapWritableState } from 'pinia';
 import { useConfig } from '@/store/configStore';
 import { ErrorCode } from "@/shared/ErrorCodes";
 import { useMain } from '@/store/mainStore';
 
 export default defineComponent({
-	components: {
-		FileInput
-	},
+	components: { FileInput, ModeSwitch },
 	data: () => ({
 		xmlinput: false,
 		compression: null,
@@ -189,41 +183,3 @@ export default defineComponent({
 	}
 });
 </script>
-
-<style>
-.p-button-success:not(:hover) .p-button-icon {
-	transition: .5s ease-out;
-	color: var(--primary-color-text) !important;
-}
-
-.home {
-	width: 100%;
-}
-.mode-switch i:before {
-	font-size: 70pt;
-}
-.mode-switch .p-togglebutton, .mode-switch .p-togglebutton * {
-	transition: .2s ease-out;
-	border: none !important;
-	background: transparent !important;
-	color: var(--primary-color) !important;
-	box-shadow: none !important;
-	font-size: 50pt !important;
-}
-.mode-switch .p-togglebutton:hover *  {
-	color: var(--text-color) !important;
-}
-.mode-switch > .icon > * {
-	position: absolute;
-	display: inline;
-	margin-top: 75pt;
-	font-weight: bold;
-	font-size: 20pt;
-	color: var(--gray-400);
-}
-.mode-switch > .icon {
-	display: flex;
-	justify-content: center;
-	padding-bottom: 35pt
-}
-</style>
